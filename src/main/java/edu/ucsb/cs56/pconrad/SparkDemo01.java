90cf27f71d87898e8cc515359985068fbbed0af2
Clare Boyle <claremboyle@umail.ucsb.edu>
2018-08-31 12:05:34 -0700

    Updated

--- src/main/java/edu/ucsb/cs56/pconrad/SparkDemo01.java
+++ src/main/java/edu/ucsb/cs56/pconrad/SparkDemo01.java
@@ -13,16 +13,22 @@ public class SparkDemo01 {
 
         port(getHerokuAssignedPort());
 		
-		System.out.println("");
-		System.out.println("(Don't worry about the warnings below about SLF4J... we'll deal with those later)");
-		System.out.println("");						  
-		System.out.println("In browser, visit: http://localhost:" + getHerokuAssignedPort() + "/hello");
-		System.out.println("");
-        spark.Spark.get("/", (req, res) -> "Hello World");
-		spark.Spark.get("/hello", (req, res) -> "Hello World");
-		spark.Spark.get("/bye", (req, res) -> "Goodbye World");
-		spark.Spark.get("/yo", (req, res) -> "S'up World");
-		spark.Spark.get("/tension", (req, res) -> "Midterm next week.  No problem dude");		
+	System.out.println("");
+	System.out.println("(Don't worry about the warnings below about SLF4J... we'll deal with those later)");
+	System.out.println("");						  
+	System.out.println("In browser, visit: http://localhost:" + getHerokuAssignedPort() + "/hello");
+	System.out.println("");
+
+	String html = "<h1><a href='/hello'>Hello</a> World!</h1>\n" +
+	    "<p>This web app is powered by \n" +
+	    "<a href='https://github.com/claremboyle/sparkjava-01'>this github repo</a></p>\n";
+	
+        spark.Spark.get("/", (req, res) -> html);
+	spark.Spark.get("/hello", (req, res) -> "<p><b>Hello, World!</b>  You just clicked the first link on my web app.</p>");
+	spark.Spark.get("/hello", (req, res) -> "Hello World");
+	spark.Spark.get("/bye", (req, res) -> "Goodbye World");
+	spark.Spark.get("/yo", (req, res) -> "S'up World");
+	spark.Spark.get("/tension", (req, res) -> "Midterm next week.  No problem dude");		
 
 	}
 	
